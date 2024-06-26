3806b967e7c5fffc8ee69ffbbfd4704c05e4f4a3
yukikimoto <ktc2243020002@edu.kyoto-tech.ac.jp>
2024-06-27 00:53:01 +0900

    FastAPIを使用した形に変更

--- function_app.py
+++ function_app.py
@@ -1,25 +1,14 @@
-import azure.functions as func
-import logging
+import azure.functions as func 
+from fastapi import FastAPI
+from api.routers import hello
 
-app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+# http://localhost:7071/ or http://localhost:7071/docs
 
-@app.route(route="api")
-def api(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+fast_app = FastAPI() 
+fast_app.include_router(hello.router)
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+@fast_app.get("/")
+async def root():
+    return {"message":"Hello world"}
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
-        )
+app = func.AsgiFunctionApp(app=fast_app, http_auth_level=func.AuthLevel.ANONYMOUS)
