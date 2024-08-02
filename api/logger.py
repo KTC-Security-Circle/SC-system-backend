c75167a3d3d7ce78acaf9725c1f6e9b5cb724516
Shieru-ktc <167487026+Shieru-ktc@users.noreply.github.com>
2024-08-03 04:03:54 +0900

    ハンドラーがない場合にのみhandlerを追加するよう変更

--- api/logger.py
+++ api/logger.py
@@ -3,8 +3,9 @@ from logging import getLogger as _getLogger, StreamHandler, INFO
 
 def getLogger(name, level: str | int = INFO):
     logger = _getLogger(name)
-    handler = StreamHandler()
-    handler.setLevel(level)
-    logger.setLevel(level)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = StreamHandler()
+        handler.setLevel(level)
+        logger.setLevel(level)
+        logger.addHandler(handler)
     return logger
