7ccc933bb313912e0ec4f00d9cde3be63d4727f6
jo <jo@jp>
2024-10-20 21:07:52 +0900

    funtoin_appからfastapiを抜き出した

--- api/app_fastapi.py
+++ api/app_fastapi.py
@@ -1,3 +1,8 @@
+from api.demo.routers import users as demo_users, chats as demo_chats, \
+    sessions as demo_sessions, error_log as demo_error_log
+from api.app.routers import users as app_users, chats as app_chats, \
+    sessions as app_sessions, error_log as app_error_log
+from api.app.routers import auth as app_auth, messages as app_messages
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
@@ -132,3 +137,21 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.get("/", tags=["root"])
+async def root():
+    return {"message": "Hello world"}
+
+# 正式なAPIのルータを登録
+app.include_router(app_auth.router, prefix="/api", tags=["api"])
+app.include_router(app_messages.router, prefix="/api", tags=["api"])
+app.include_router(app_users.router, prefix="/api", tags=["api"])
+app.include_router(app_chats.router, prefix="/api", tags=["api"])
+app.include_router(app_sessions.router, prefix="/api", tags=["api"])
+app.include_router(app_error_log.router, prefix="/api", tags=["api"])
+
+# デモ用APIのルータを登録
+app.include_router(demo_users.router, prefix="/demo", tags=["demo"])
+app.include_router(demo_chats.router, prefix="/demo", tags=["demo"])
+app.include_router(demo_sessions.router, prefix="/demo", tags=["demo"])
+app.include_router(demo_error_log.router, prefix="/demo", tags=["demo"])
