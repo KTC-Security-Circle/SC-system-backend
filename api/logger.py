63da58619f71d3e4c7ac4a3c65f75a7a6e9c1e26
jo <jo@.com>
2024-12-05 06:12:39 +0000

    基本ファイルの型注釈,コメントの追加

--- api/logger.py
+++ api/logger.py
@@ -1,21 +1,39 @@
-from logging import INFO, Formatter, StreamHandler
+# 標準ライブラリ
+from logging import INFO, Formatter, Logger, StreamHandler
 from logging import getLogger as _getLogger
 
 
-def getLogger(name, level: str | int = INFO):
+def getLogger(name: str, level: str | int = INFO) -> Logger:
+    """
+    カスタムロガーを取得または作成します。
+
+    Args:
+        name (str): ロガーの名前。
+        level (Union[str, int], optional): ログレベル。デフォルトは INFO。
+
+    Returns:
+        Logger: カスタム設定されたロガー。
+    """
     logger = _getLogger(name)
+
+    # ハンドラーがまだ追加されていない場合のみ設定
     if not logger.handlers:
         # 標準出力用のハンドラーを作成
         handler = StreamHandler()
+
+        # ログレベルが無効な場合はエラー
+        if not isinstance(level, str | int):
+            raise ValueError(f"Invalid log level: {level}")
+
+        # ハンドラーのレベルを設定
         handler.setLevel(level)
 
         # 一般的なフォーマットを設定
-        formatter = Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        )
+        formatter = Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         handler.setFormatter(formatter)
 
         # ロガーにハンドラーを追加
         logger.setLevel(level)
         logger.addHandler(handler)
+
     return logger
--- function_app.py
+++ function_app.py
@@ -2,7 +2,9 @@ import logging
 
 import azure.functions as func
 
-from api.app_fastapi import app
+from api.demo_fastapi import app as demo_app
+
+# from api.app_fastapi import app as app_app
 from api.logger import getLogger
 
 # ロガーを設定し、ログレベルをDEBUGに設定
@@ -11,5 +13,5 @@ logger.setLevel(logging.DEBUG)
 
 # FastAPIアプリをAzure FunctionsのASGIアプリケーションとして設定
 function_app = func.AsgiFunctionApp(
-    app=app, http_auth_level=func.AuthLevel.ANONYMOUS
+    app=demo_app, http_auth_level=func.AuthLevel.ANONYMOUS
 )
