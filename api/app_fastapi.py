4c23ad619b8e8c078102c894ee7c19e02d5ce992
jo <jo@.com>
2024-12-18 14:31:25 +0000

    動くようになったのでそのままpushします

--- api/app_fastapi.py
+++ api/app_fastapi.py
@@ -2,9 +2,14 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import SQLModel
 
 from api.app.database.engine import get_engine
+from api.app.routers.auth import router as auth_app_router
+from api.app.routers.chats import router as chat_app_router
+from api.app.routers.sessions import router as session_app_router
+from api.app.routers.users import router as user_app_router
 from api.demo.routers.chats import router as chatlog_demo_router
 from api.demo.routers.sessions import router as session_demo_router
 from api.demo.routers.users import router as user_demo_router
@@ -28,25 +33,25 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-# origins = [
-#     "http://localhost",
-#     "http://localhost:3000",
-#     "http://localhost:7071",
-#     "https://sc-test-api.azurewebsites.net",
-# ]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:7071",
+    "https://sc-test-api.azurewebsites.net",
+]
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-# app.include_router(auth_router, prefix="/auth")
-# app.include_router(user_router, prefix="/api")
-# app.include_router(session_router, prefix="/api")
-# app.include_router(chatlog_router, prefix="/api")
-app.include_router(user_demo_router, prefix="/api")
-app.include_router(session_demo_router, prefix="/api")
-app.include_router(chatlog_demo_router, prefix="/api")
+app.include_router(auth_app_router, prefix="/auth")
+app.include_router(user_app_router, prefix="/api")
+app.include_router(session_app_router, prefix="/api")
+app.include_router(chat_app_router, prefix="/api")
+# app.include_router(user_demo_router, prefix="/api")
+# app.include_router(session_demo_router, prefix="/api")
+# app.include_router(chatlog_demo_router, prefix="/api")
